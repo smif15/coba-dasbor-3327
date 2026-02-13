c81c8f9f0a9daf29396ade146cae458af8da4f97
Faris Iqbal <farisiqbalms15@gmail.com>
2026-02-14 06:54:23 +0700

    v1.0 minimalis, coba dulu

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+
+const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
+
+const app = express();
+
+app.use(express.json()) 
+
+app.use(express.static(path.join(__dirname, 'dist')))
+
+app.post('/api/run', (req, res) => {
+    const { url } = req.body;
+
+    axios.get(`${url}/${API_KEY}`)
+        .then(response => {
+            const { turvar, tahun, turtahun, datacontent } = response.data;
+            res.json({ vari : response.data.var, turvar, tahun, turtahun, datacontent })
+        })
+})
+
+
+app.listen(PORT, () => {
+    console.log('App is running...');
+})
